313b7478f412d319a70ee310f437be0886a511bf
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2022-05-12 16:45:21 +0200

    export components

--- src/entry.esm.js
+++ src/entry.esm.js
@@ -1,16 +1,3 @@
-// Import vue components
-import * as components from './components/index.js';
-
-// install function executed by Vue.use()
-const install = function installDigiwfFormRenderer(Vue) {
-  Object.entries(components).forEach(([componentName, component]) => {
-    Vue.component(componentName, component);
-  });
-};
-
-// Create module definition for Vue.use()
-export default install;
-
 // To allow individual component use, export components
 // each can be registered via Vue.component()
 export * from './components/index.js';
