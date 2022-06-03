5bc4f9660daabf008cfa866412f5aa4e78c79e1f
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2022-06-03 15:28:11 +0200

    show provider display name

--- src/store/index.js
+++ src/store/index.js
@@ -47,7 +47,9 @@ const store = new Vuex.Store({
                                     return provider.id === relation.provider.id
                                 })[0]
 
-                                relation.provider.name = foundProvider.data.displayName ?? foundProvider.name
+                                relation.provider.name = foundProvider.data.displayName
+                                    ? foundProvider.data.displayName
+                                    : foundProvider.name
                                 relation.provider.slots = relation.slots
 
                                 request.providers.push(relation.provider)
