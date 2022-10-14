e1e4b41d3dd9b6b228f9b94b29ba0e0fc1ad970f
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2022-10-14 09:07:26 +0200

    test

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,3 @@ export { default as AppointmentForm } from './AppointmentForm.vue';
 export { default as Calendar } from './Calnedar.vue';
 export { default as SwitchLanguage } from './SwitchLanguage.vue';
 export { default as ServiceFinder } from './ServiceFinder.vue';
-export { default as CustomerInfo } from './CustomerInfo.vue';
