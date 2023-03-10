7226c90f3ef7c332f898dc51d02b362309f02a81
Waldemar Kowalenko <ext.dl.kowalenko@muenchen.de>
2023-03-10 08:57:49 +0100

    ZMS-710

--- src/components/AppointmentForm.vue
+++ src/components/AppointmentForm.vue
@@ -334,7 +334,7 @@ export default {
     cancelDialog: false,
     starOverDialog: false,
     appointmentCancelled: null,
-    activatedAppointment: false
+    activatedAppointment: null
   }),
   computed: {
     appointmentCanBeConfirmed() {
