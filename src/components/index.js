0bed68a89a9baaef2c589d8e99ad230cd5d12eb2
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2023-04-12 13:04:58 +0200

    feat(ZMS-905): fix build

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable*/
 export { default as AppointmentForm } from './AppointmentForm.vue';
 export { default as Calendar } from './Calnedar.vue';
-export { default as store } from './../store';
-export { default as translations } from './../translations';
+export { default as store } from './../store/index.js';
+export { default as translations } from './../translations/index.js';
 export { default as SwitchLanguage } from './SwitchLanguage.vue';
 export { default as ServiceFinder } from './ServiceFinder.vue';
 export { default as CustomerInfo } from './CustomerInfo.vue';
