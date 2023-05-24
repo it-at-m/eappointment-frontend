69b834bf8053db9b34befb124ca10da2ca04e252
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2023-05-24 10:36:21 +0200

    feat(ZMS-1051): fix lint

--- src/store/modules/formData/mutations.js
+++ src/store/modules/formData/mutations.js
@@ -14,6 +14,7 @@ export default {
     },
     // eslint-disable-next-line no-unused-vars
     reset (state) {
+        // eslint-disable-next-line no-unused-vars
         state = {
             service: null,
             appointment: null,
