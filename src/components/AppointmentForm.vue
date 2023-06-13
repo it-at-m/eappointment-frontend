c11c76d629864e54108726f6425351c183f2603a
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2023-06-13 12:03:19 +0200

    bugfix(ZMS-970): fix messaging

--- src/components/AppointmentForm.vue
+++ src/components/AppointmentForm.vue
@@ -207,7 +207,7 @@
 
               <v-alert
                   class="appointment-activated"
-                  v-if="activatedAppointment && !appointmentCancelled && !$store.state.isRebooking && !confirmedAppointment"
+                  v-if="activatedAppointment && !appointmentCancelled"
                   :color="$store.state.settings.theme.success"
               >
                 {{ $t('appointmentIsConfirmed') }}
